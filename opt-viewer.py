daaece28d913a8284ddf31a77cf30abe04fa49a6
David Malcolm <dmalcolm@redhat.com>
2018-05-16 20:47:22 -0400

    Start adding bootstrap CSS/JS

--- opt-viewer.py
+++ opt-viewer.py
@@ -79,15 +79,46 @@ def write_td_count(f, record):
 def url_from_location(loc):
     return '%s#line-%i' % (srcfile_to_html(loc['file']), loc['line'])
 
+def write_html_header(f, title, head_content):
+    """
+    Write initial part of HTML file using Bootstrap, up to and including
+    opening of the <body> element.
+    """
+    f.write('<!doctype html>\n'
+            '<html lang="en">\n'
+            '  <head>\n'
+            '    <!-- Required meta tags -->\n'
+            '    <meta charset="utf-8">\n'
+            '    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">\n'
+            '\n'
+            '    <!-- Bootstrap CSS -->\n'
+            '    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">\n'
+            '\n')
+    f.write(head_content)
+    f.write('    <title>%s</title>\n' % title)
+    f.write('  </head>\n'
+            '  <body>\n')
+
+def write_html_footer(f):
+    """
+    Write final part of HTML file using Bootstrap, closing the </body>
+    element.
+    """
+    # jQuery first, then Popper.js, then Bootstrap JS
+    f.write('    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>\n'
+            '    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>\n'
+            '    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>\n'
+            '  </body>\n'
+            '</html>\n')
+
 def make_index_html(out_dir, records):
     # Sort by highest-count down to lowest-count
     records = sorted(records, key=record_sort_key)
 
-    filename = os.path.join(out_dir, "index.html");
+    filename = os.path.join(out_dir, "index.html")
     with open(filename, "w") as f:
-        f.write('<html>\n')
-        f.write('<body>\n')
-        f.write('<table>\n')
+        write_html_header(f, 'Optimizations', '')
+        f.write('<table class="table table-striped table-bordered">\n')
         f.write('  <tr>\n')
         f.write('    <th>Source Location</th>\n')
         f.write('    <th>Execution Count</th>\n')
@@ -113,26 +144,25 @@ def make_index_html(out_dir, records):
             # Function:
             f.write('    <td>\n')
             f.write(html.escape(record['function']))
-            f.write('    <td>\n')
+            f.write('    </td>\n')
 
             # Pass:
             write_td_pass(f, record)
 
             f.write('  </tr>\n')
         f.write('</table>\n')
-        f.write('</body>\n')
-        f.write('</html>\n')
+        write_html_footer(f)
 
 def get_html_for_message(record):
     html_for_message = ''
     for item in record['message']:
         if type(item) is dict:
-            text = ''
             if 'expr' in item:
-                text = item['expr']
+                html_for_item = '<code>%s</code>' % html.escape(item['expr'])
             elif 'stmt' in item:
-                text = item['stmt']
-            html_for_item = html.escape(text)
+                html_for_item = '<code>%s</code>' % html.escape(item['stmt'])
+            else:
+                html_for_item = ''
             if 'location' in item:
                 loc = item['location']
                 html_for_item = ('<a href="%s">%s</a>'
@@ -163,15 +193,6 @@ def make_per_source_file_html(build_dir, out_dir, records):
     # Write style.css
     with open(os.path.join(out_dir, "style.css"), "w") as f:
         f.write(formatter.get_style_defs())
-        f.write('''
-table {
-    border-collapse: collapse;
-}
-
-table, td, th, tr {
-    border: 1px solid lightgrey;
-}
-''')
 
     for src_file in by_src_file:
         if 0:
@@ -217,11 +238,10 @@ table, td, th, tr {
             by_line_num[line_num].append(record)
 
         with open(os.path.join(out_dir, srcfile_to_html(src_file)), "w") as f:
-            f.write('<html>\n')
-            f.write('<link rel="stylesheet" href="style.css" type="text/css" />\n')
-            f.write('<body>\n')
+            write_html_header(f, html.escape(src_file),
+                              '<link rel="stylesheet" href="style.css" type="text/css" />\n')
             f.write('<h1>%s</h1>' % html.escape(src_file))
-            f.write('<table>\n')
+            f.write('<table class="table table-striped table-bordered">\n')
             f.write('  <tr>\n')
             f.write('    <th>Line</th>\n')
             f.write('    <th>Hotness</th>\n')
@@ -300,8 +320,7 @@ table, td, th, tr {
                     f.write('  </tr>\n')
 
             f.write('</table>\n')
-            f.write('</body>\n')
-            f.write('</html>\n')
+            write_html_footer(f)
 
 def make_html(build_dir, out_dir, records):
     if not os.path.exists(out_dir):
